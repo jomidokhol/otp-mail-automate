566340f211e1151b97068341dd8f55ab00bff4a9
jomidokhol <grownm.yt@gmail.com>
2026-05-07 20:57:02 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint warnings or errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Ignores typescript errors
+    ignoreBuildErrors: true,
+  },
+  // Automatically fix some hanging issues in API routes
+  experimental: {
+    serverComponentsExternalPackages: ['nodemailer']
+  }
+}
+
+module.exports = nextConfig;
